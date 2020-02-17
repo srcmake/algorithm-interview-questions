025651661e0c7b72aae92041685c58e3135ea441
Tariq Ramlall <srcmake@gmail.com>
2020-02-17 08:30:56 -0500

    Finished code for NextGreaterElement.

--- NextGreaterElementInArray/main.cpp
+++ NextGreaterElementInArray/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
+////////////////////////////////////////////////////////////////////////////////
 // Print the elements in the vector.
 void PrintArray(vector<int> A)
 	{
@@ -12,14 +14,67 @@ void PrintArray(vector<int> A)
 		}
 	cout << endl;
 	}
+////////////////////////////////////////////////////////////////////////////////
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Return an array that contains the Next Greater Element for each element in A.
 vector<int> NextGreaterElement(vector<int> A)
 	{
 	vector<int> ans(A.size());
 
+	// Create a stack.
+	stack<int> s;
+
+	// For each element in A...
+	for(int i = 0; i < A.size(); i++)
+		{
+		// If the stack is empty, then just push the index of the element onto the stack.
+		if(s.empty() == true)
+			{
+			s.push(i);
+			continue;
+			}
+
+		// Otherwise, check if this element is the NGE for the elements currently on the stack.
+    while(s.empty() == false)
+      {
+      int topOfStackIndex = s.top();
+      
+      // The current element is greater than the stack's element...
+      if(A[i] > A[topOfStackIndex])
+        {
+        // Mark this element as the NGE for this index.
+        ans[topOfStackIndex] = A[i];
+        
+        // Pop that index off the stack since it has its NGE now.
+        s.pop();
+        }
+      // The current element isn't better, so stop checking.
+      else
+        {
+        break;
+        }
+			}
+		
+		// We're done checking if this element is the NGE of it's left neighbors so push its index on the stack.
+		s.push(i);
+		}
+  
+  // Cleanup: Any elements that were left on the stack are -1.
+  while(s.empty() == false)
+    {
+    int topOfStackIndex = s.top();
+    ans[topOfStackIndex] = -1;
+    s.pop();
+		}
+
 	return ans;
 	}
+////////////////////////////////////////////////////////////////////////////////
 
+
+////////////////////////////////////////////////////////////////////////////////
 int main()
 	{
 	std::cout << "Program started.\n";
@@ -40,3 +95,4 @@ int main()
 	
 	std::cout << "Program ended.\n";
 	}
+////////////////////////////////////////////////////////////////////////////////
